70d94ad72645c4d2ea316fc338da5a4e27f6e3e0
Xingjian <i@xingjian.me>
2017-03-08 23:46:50 +0800

    minor

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
-
-        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style',
+                  'owner')
+    owner = serializers.ReadOnlyField(source='owner.username')
 
 
 # Serializers define the API representation.
