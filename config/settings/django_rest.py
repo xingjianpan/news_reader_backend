db6aace042a5bf06eb4e02ac6a21f32d4ebf4caf
Xingjian <i@xingjian.me>
2017-03-19 09:40:11 +0800

    minor

--- config/settings/django_rest.py
+++ config/settings/django_rest.py
@@ -24,3 +24,15 @@ CORS_ALLOW_METHODS = (
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
